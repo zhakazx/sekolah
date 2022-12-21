adb21642eef62356385dad950195a198a01283c3
Zhaka-Hidayat <zhakazx@gmail.com>
2022-12-21 18:47:04 +0800

    chore: layout basic for the dashboard

--- client/src/layout/index.jsx
+++ client/src/layout/index.jsx
@@ -1,14 +1,140 @@
-import React from 'react'
-import Sidebar from '../components/Sidebar'
-import { Outlet } from 'react-router-dom'
+import React, { useState } from "react";
+import { HiMenuAlt3 } from "react-icons/hi";
+import { MdOutlineDashboard } from "react-icons/md";
+import { RiSettings4Line } from "react-icons/ri";
+import { TbReportAnalytics } from "react-icons/tb";
+import { AiOutlineUser, AiOutlineHeart } from "react-icons/ai";
+import { FiMessageSquare, FiFolder, FiShoppingCart } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 const Layout = () => {
+  const menus = [
+    { name: "Dashboard", link: "/dashboard", icon: MdOutlineDashboard },
+    { name: "Berita", link: "/berita", icon: FiFolder },
+  ];
+  const [open, setOpen] = useState(false);
   return (
-    <div>
-        <Sidebar />
-        <Outlet />
-    </div>
-  )
-}
+    <div className="sidebar flex min-h-screen">
+      <div
+        className={`bg-[#0e0e0e] relative z-30 ${
+          open ? "w-72" : "w-16"
+        } duration-500 text-gray-100 px-4`}
+      >
+        <div className={`py-3 flex justify-end`}>
+          <HiMenuAlt3
+            size={26}
+            className="cursor-pointer"
+            onClick={() => setOpen(!open)}
+          />
+        </div>
+        <div className="mt-4 flex flex-col gap-4 relative">
+          {menus?.map((menu, i) => (
+            <Link
+              to={menu?.link}
+              key={i}
+              className={` ${
+                menu?.margin && "mt-5"
+              } group flex items-center text-sm  gap-3.5 font-medium p-2 hover:bg-gray-800 rounded-md`}
+            >
+              <div>{React.createElement(menu?.icon, { size: "20" })}</div>
+              <h2
+                style={{
+                  transitionDelay: `${i + 3}00ms`,
+                }}
+                className={`whitespace-pre duration-500 ${
+                  !open && "opacity-0 translate-x-28 overflow-hidden"
+                }`}
+              >
+                {menu?.name}
+              </h2>
+              <h2
+                className={`${
+                  open && "hidden"
+                } absolute left-48 bg-white font-semibold whitespace-pre text-gray-900 rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-14 group-hover:duration-300 group-hover:w-fit  `}
+              >
+                {menu?.name}
+              </h2>
+            </Link>
+          ))}
+        </div>
+      </div>
 
-export default Layout
+    <main class="main flex flex-col flex-grow md:ml-0 transition-all duration-150 ease-in">
+      <header class="header bg-white shadow py-4 px-4">
+        <div class="header-content flex items-center flex-row">
+          <form action="#">
+            <div class="hidden md:flex relative">
+              <div
+                class="inline-flex items-center justify-center absolute left-0 top-0 h-full w-10 text-gray-400"
+              >
+                <svg
+                  class="h-6 w-6"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                </svg>
+              </div>
+
+              <input
+                id="search"
+                type="text"
+                name="search"
+                class="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-300 w-full h-10 focus:outline-none focus:border-indigo-400"
+                placeholder="Search..."
+              />
+            </div>
+            <div class="flex md:hidden">
+              <a href="#" class="flex items-center justify-center h-10 w-10 border-transparent">
+                <svg
+                  class="h-6 w-6 text-gray-500"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                </svg>
+              </a>
+            </div>
+          </form>
+          <div class="flex ml-auto">
+            <a href class="flex flex-row items-center">
+              <img
+                src="https://pbs.twimg.com/profile_images/378800000298815220/b567757616f720812125bfbac395ff54_normal.png"
+                alt
+                class="h-10 w-10 bg-gray-200 border rounded-full"
+              />
+              <span class="flex flex-col ml-2">
+                <span class="truncate w-20 font-semibold tracking-wide leading-none">John Doe</span>
+                <span class="truncate w-20 text-gray-500 text-xs leading-none mt-1">Manager</span>
+              </span>
+            </a>
+          </div>
+        </div>
+      </header>
+      <div class="main-content flex flex-col flex-grow p-4">
+        <h1 class="font-bold text-2xl text-gray-700">Dashboard</h1>
+
+        <div
+          class="flex flex-col flex-grow border-4 border-gray-400 border-dashed bg-white rounded mt-4"
+        ></div>
+      </div>
+      <footer class="footer px-4 py-6">
+        <div class="footer-content">
+          <p class="text-sm text-gray-600 text-center">© Brandname 2020. All rights reserved. <a href="https://twitter.com/iaminos">by iAmine</a></p>
+        </div>
+      </footer>
+    </main>
+      
+    </div>
+  );
+};
+
+export default Layout;
